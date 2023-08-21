c489a22ba958785492bf5ae2449346c88d81920f
Dandan Drori <dandan.drori@zoominfo.com>
2023-08-21 12:44:51 +0300

    fix: remove all testing code and refactor

--- services/tickets.js
+++ services/tickets.js
@@ -4,18 +4,18 @@ export async function startTicketsCheck() {
     const endDate = new Date('08-28-2023, 18:00').getTime();
     const intervalId = setInterval(async () => {
         try {
-            const isTicketsAvailableForPurchase = await getTicketsStatus();
-            if (!isTicketsAvailableForPurchase) {
-                const message = 'TEST';
-                // const message = `Your tickets are ready for purchase!\nGo to ${process.env.TICKETS_URL}`;
-                await sendTelegramMessage(message);
-                clearInterval(intervalId);
-            }
             if (Date.now() >= endDate) {
+                clearInterval(intervalId);
+                throw new Error('Event already started!');
+            }
+            const isTicketsAvailableForPurchase = await getTicketsStatus();
+            if (isTicketsAvailableForPurchase) {
+                const message = `Your tickets are ready for purchase!\nGo to ${process.env.TICKETS_URL}`;
+                await sendTelegramMessage(message);
                 clearInterval(intervalId);
             }
         } catch (err) {
             console.log(err);
         }
     }, 1000 * 60 * 30);
-};
+}
